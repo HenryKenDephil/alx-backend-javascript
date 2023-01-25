f4f5fd5ddb60d79213ae6f37b259c12917aa72b6
HenryKenDephil <h.komoloh@gmail.com>
2023-01-25 20:50:39 +0300

    promising to get api response

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI(){
+    /*eslint-disabled*/
+    return new Promise((resolve, reject) => {
+        console.log("retrieved response from api");
+        resolve();
+    }),
+};
+export default getResponseFromAPI;
